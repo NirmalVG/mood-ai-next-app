8b246bcefb451422c0c9c0b335a2a261474919e5
Nirmal V G <nirmalvg97@gmail.com>
2024-10-27 19:43:41 +0530

    fixes

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -3,77 +3,91 @@ import { updateEntry } from "@/utils/api";
 import { useState } from "react";
 import { useAutosave } from "react-autosave";
 import Spinner from "./Spinner";
+import { useRouter } from "next/navigation";
 
-const Editor = ({ entry }: any) => {
-    const [value, setValue] = useState(entry.content);
-    const [isLoading, setIsLoading] = useState(false);
-    const [analysis, setAnalysis] = useState(entry.analysis);
+interface Entry {
+    id: string;
+    content: string;
+    analysis: {
+        color: string;
+        subject: string;
+        mood: string;
+        negative: boolean;
+    };
+}
 
-    const { mood, summary, color, negative, subject } = analysis || {};
-    const AnalysisData = [
-        {
-            name: "Summary",
-            value: summary,
-        },
-        {
-            name: "Subject",
-            value: subject,
-        },
-        {
-            name: "Mood",
-            value: mood,
-        },
-        {
-            name: "Negative",
-            value: negative ? "True" : "False",
-        },
-    ];
+const Editor = ({ entry }: { entry: Entry }) => {
+    const [text, setText] = useState(entry.content);
+    const [currentEntry, setEntry] = useState(entry);
+    const [isSaving, setIsSaving] = useState(false);
+    const router = useRouter();
 
     useAutosave({
-        data: value,
-        onSave: async (_value) => {
-            setIsLoading(true);
-            const data = await updateEntry(entry.id, _value);
-            setAnalysis(data.analysis);
-            setIsLoading(false);
+        data: text,
+        onSave: async (_text) => {
+            if (_text === entry.content) return;
+            setIsSaving(true);
+
+            const { data } = await updateEntry(entry.id, { content: _text });
+
+            setEntry(data);
+            setIsSaving(false);
         },
     });
+
     return (
         <div className="w-full h-full grid grid-cols-3 gap-0 relative">
             <div className="absolute left-0 top-0 p-2">
-                {isLoading ? (
+                {isSaving ? (
                     <Spinner />
                 ) : (
                     <div className="w-[16px] h-[16px] rounded-full bg-green-500"></div>
                 )}
-                <div className="col-span-2">
-                    <textarea
-                        className="w-full h-full text-xl p-8"
-                        value={value}
-                        onChange={(e) => setValue(e.target.value)}
-                    />
-                </div>
             </div>
-
+            <div className="col-span-2">
+                <textarea
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    className="w-full h-full text-xl p-8"
+                />
+            </div>
             <div className="border-l border-black/5">
-                <div className="h-[100px] bg-blue-600 text-white p-8" style={{ backgroundColor: color }}>
-                    <h2 className="text-2xl bg-white/25 text-black">Analysis</h2>
+                <div
+                    style={{ background: currentEntry.analysis.color }}
+                    className="h-[100px] bg-blue-600 text-white p-8"
+                >
+                    <h2 className="text-2xl bg-white/25 text-black">
+                        Analysis
+                    </h2>
                 </div>
                 <div>
                     <ul role="list" className="divide-y divide-gray-200">
-                        {AnalysisData.map((item) => {
-                            return (
-                                <li
-                                    key={item.name}
-                                     className="py-4 px-8 flex items-center justify-between"
-                                >
-                                    <div className="text-xl font-semibold">
-                                        {item.name}
-                                    </div>
-                                    <div className="text-xl">{item.value}</div>
-                                </li>
-                            );
-                        })}
+                        <li className="py-4 px-8 flex items-center justify-between">
+                            <div className="text-xl font-semibold w-1/3">
+                                Subject
+                            </div>
+                            <div className="text-xl">
+                                {currentEntry.analysis.subject}
+                            </div>
+                        </li>
+
+                        <li className="py-4 px-8 flex items-center justify-between">
+                            <div className="text-xl font-semibold">Mood</div>
+                            <div className="text-xl">
+                                {currentEntry.analysis.mood}
+                            </div>
+                        </li>
+
+                        <li className="py-4 px-8 flex items-center justify-between">
+                            <div className="text-xl font-semibold">
+                                Negative
+                            </div>
+                            <div className="text-xl">
+                                {currentEntry.analysis.negative
+                                    ? "True"
+                                    : "False"}
+                            </div>
+                        </li>
                     </ul>
                 </div>
             </div>
